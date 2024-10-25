3af93f0c81ea0ade33cc7be3c78b8f14137d9338
JeffSeirre <strakmi@gmail.com>
2024-10-25 17:12:17 +0100

    add training_agent class

---
+++ Backend/training_agent.py
@@ -0,0 +1,11 @@
+from game import *
+
+class Training_agent():
+    def __init__(self, agent, size, connect, opponent_type="random") -> None:
+        self.game = Game(size, connect)
+        self.agent = agent
+        self.opponent_type = opponent_type
+
+    def training_loop():
+        pass
+
