f6d9445919a4aed5e71c00f71b4bef94954f752e
JeffSeirre <strakmi@gmail.com>
2024-11-02 14:23:32 +0000

    Add Q_net class

---
+++ Backend/networks.py
@@ -0,0 +1,19 @@
+import numpy as np
+import torch as t
+import torch.nn as nn
+
+class Q_net(nn.Module):
+    def __init__(self, board_size, channels = [1,4,8,16,1], kernel_sizes = [5, 5, 4, 3]):
+
+        self.network = nn.Sequential(
+            nn.Conv2d(in_channels=channels[0], out_channels=channels[1], kernel_size=kernel_sizes[0], padding_mode="zeros"),
+            nn.Conv2d(in_channels=channels[1], out_channels=channels[2], kernel_size=kernel_sizes[1], padding_mode="zeros"),
+            nn.Conv2d(in_channels=channels[2], out_channels=channels[3], kernel_size=kernel_sizes[2], padding_mode="zeros"),
+            nn.Conv2d(in_channels=channels[3], out_channels=channels[4], kernel_size=kernel_sizes[3], padding_mode="zeros"),
+            nn.Softmax2d()
+        )
+
+        super.init()
+
+    def forward(self, input):
+        return self.network.forward(input=input)
