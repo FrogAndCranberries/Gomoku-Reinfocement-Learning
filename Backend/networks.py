13abc1f51c4abe3200a0eede0fd703ef4fd0cae5
JeffSeirre <strakmi@gmail.com>
2024-11-04 16:01:23 +0000

    Generalise NN construction for an arbitrary number of layers and intercalate ReLUs

--- Backend/networks.py
+++ Backend/networks.py
@@ -1,19 +1,30 @@
 import numpy as np
 import torch as t
 import torch.nn as nn
+from itertools import chain
+from typing import Iterable
 
 class Q_net(nn.Module):
     """
     A convolutional network that takes a game board and returns the probabilities for the best next move.
     """
-    def __init__(self, channels, kernel_sizes):
+    def __init__(self, channels:Iterable[int], kernel_sizes:Iterable[int]) -> None:
 
         super().__init__()
-        self.network = nn.Sequential(
-            nn.Conv2d(in_channels=channels[0], out_channels=channels[1], kernel_size=kernel_sizes[0], 
-                      padding=(kernel_sizes[0] - 1) // 2, padding_mode="zeros"),
-            nn.Softmax2d()
-        )
+
+        # Generate alternating Conv2d and ReLU layers using passed channel and kernel sizes
+        layers = list(chain.from_iterable([
+            [nn.Conv2d(in_channels=channels[index], out_channels=channels[index + 1], kernel_size=kernel, 
+                padding=(kernel - 1) // 2, padding_mode="zeros"),
+                nn.ReLU()]
+            for index, kernel in enumerate(kernel_sizes)]))
+        
+        # Replace last ReLU with a 2d Softmax and create a sequential module
+        layers.pop()
+
+        layers.append(nn.Softmax2d())
+
+        self.network = nn.Sequential(layers)
 
     def forward(self, input:t.Tensor):
         return self.network.forward(input)
